0d4a0cba9ef5e22c1b2653c3ea1a4af8cd817186
interjc <interjc@cbc685ec-1e98-11de-8b5c-13accb87f508>
2010-01-21 11:26:31 +0000

    修改了sidebar默认排布

--- trunk/shadowblue/sidebar-3.php
+++ trunk/shadowblue/sidebar-3.php
@@ -3,18 +3,7 @@
         <?php if(function_exists('dynamic_sidebar') && dynamic_sidebar(3)):else: ?>
         <?php get_links_list();?>
         <li id="calendar" class="widget widget_calendar">
-          <h2>日历</h2>
           <?php get_calendar(); ?>
-        </li>
-        <li class="widget widget_meta">
-            <h2>选项</h2>
-            <ul>
-                <?php wp_register(); ?>
-                <li>
-                    <?php wp_loginout(); ?>
-                </li>
-                <?php wp_meta(); ?>
-            </ul>
         </li>
         <?php endif;?>
     </ul>
--- trunk/shadowblue/sidebar.php
+++ trunk/shadowblue/sidebar.php
@@ -1,13 +1,32 @@
 <div id="sidebar-1" class="sidebar">
     <ul class="top-level">
         <?php if(function_exists('dynamic_sidebar') && dynamic_sidebar(1)):else: ?>
+        <li id="calendar" class="widget widget_calendar">
+          <?php get_calendar(); ?>
+        </li>
         <li id="search" class="widget">
             <?php include(TEMPLATEPATH . '/searchform.php'); ?>
         </li>
-        <li class="widget widget_archive">
-            <h2>存档</h2>
+        <li id="postbypost" class="widget widget_recent_entries">
+          <h2>最新文章</h2>
+          <ul>
+            <?php wp_get_archives('type=postbypost&limit=10'); ?>
+          </ul>
+        </li>
+        <li>
+          <h2>标签</h2>
+          <ul>
+            <?php wp_tag_cloud('smallest=8&largest=22'); ?>
+          </ul>
+        </li>
+        <li class="widget widget_meta">
+            <h2>选项</h2>
             <ul>
-                <?php wp_get_archives('type=monthly');?>
+                <?php wp_register(); ?>
+                <li>
+                    <?php wp_loginout(); ?>
+                </li>
+                <?php wp_meta(); ?>
             </ul>
         </li>
         <?php endif;?>
