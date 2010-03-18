054983b82acc2517ae41522c62f68d0779c3ef67
interjc <interjc@cbc685ec-1e98-11de-8b5c-13accb87f508>
2010-03-18 06:28:01 +0000

    Shadowblue 0.5 beta 发行版

--- trunk/shadowblue/nav.php
+++ trunk/shadowblue/nav.php
@@ -1,5 +1,5 @@
 <div id="nav" class="solo">
-  <?php switchCatOrTag('cat'); //如果需要导航栏显示Tags，请将cat替换为tag ?> 
+  <?php switchCatOrTag($blogOption['nav_display']); //如果需要导航栏显示Tags，请将cat替换为tag ?> 
   <a class="switch" title="更多" href="#">更多</a>
 </div>
 <!--/nav-->
